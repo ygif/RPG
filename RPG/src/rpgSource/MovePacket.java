efd3c32a3d32b8a8e9b7a44f755986ef0cadd143
ygif <hailey_goldschmidt@cox.net>
2017-10-30 21:25:52 -0700

    Move now does what PreConNumMove did and adjusted internal selector

--- RPG/src/rpgSource/MovePacket.java
+++ RPG/src/rpgSource/MovePacket.java
@@ -1,28 +1,31 @@
 package rpgSource;
 
 import rpgSource.entity.Entities;
-import rpgSource.moves.Move;
+import rpgSource.moves.PreConNumMove;
 
-public class MovePacket extends Packet {
-	
-	Move move;
+public class ConditionalMovePacket extends Packet {
 
-	public MovePacket(Entities u, Entities t, Move m) {
+	PreConNumMove move;
+
+	public ConditionalMovePacket(Entities u, Entities t, PreConNumMove m) {
 		super(u, t);
 		move = m;
 	}
 	
-	public MovePacket(Entities e, Move m) {
-		super(e, e);
+	public ConditionalMovePacket(Entities e, PreConNumMove m) {
+		super(e);
 		move = m;
 	}
 
 	@Override
 	public void apply() {
+		move.precondition(user.getMp());
 		user.message(move.getDes());
 		double damage = move.getBaseDamage() * ((double) user.getAttack()/10);
-		target.reduceHealth(damage);
-		Entities.ui.appendToConsole("The " + target.getName() + " has " + Entities.numberPrinter.format(target.getCurrentHealth()) + " health.\n");
-		move.doSomething();
+		if(damage > 0) {
+			target.reduceHealth(damage);
+			user.message("The " + target.getName() + " has " + Entities.numberPrinter.format(target.getCurrentHealth()) + " health.");
+			move.doSomething();
+		}
 	}
 }
